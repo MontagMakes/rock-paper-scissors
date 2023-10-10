1884583a8de2be45be5c4a0f4ea2cd1117c5238f
SolitaryXero <103876121+SolitaryXero@users.noreply.github.com>
2023-10-10 11:02:49 +0500

    change into TOP algorithm completed

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 //alternative branch
-addEventListener
-
 //Create function named "getComputerChoice" to get Computer's choice
 function getComputerChoice(){
     //  Create variable "computerChoice" of type string
@@ -19,64 +17,66 @@ function getComputerChoice(){
     }
     return computerChoice;
 }
-//Create a variable named "computerChoice" of type string
-//store "rock", "paper" or "scissors" in the computer variable
-let computerChoice = getComputerChoice();
 
 //Create a function "getUserChoice"
 function getUserChoice(){
     //Create a variable "userChoice"
     //Prompt the User to choose between "rock", "paper" or "scissors"
-    let userChoice = prompt("Please Submit '1' to choose 'rock', '2' to choose 'paper' and '3' to choose 'scissors'");
+    let userChoice = prompt("Please choose one of the 3 options, Rock, Paper or scissors.").toLowerCase();
     
     //check if the user input is correct
     //Return user's choice
-    if (userChoice === "1"){
+    if (userChoice === "rock"){
         return "rock"
-    } else if (userChoice === "2"){
+    } else if (userChoice === "paper"){
         return "paper"
-    } else if (userChoice === "3"){
+    } else if (userChoice === "scissors"){
         return "scissors"
     } else {
         console.log("Invalid Input, Please refresh the page and play again.");
     }
 }
 
-//Create a variable named "userChoice" for User's choice
-//store user's choice in "userChoice"
-let userChoice = getUserChoice();
 
 //Create a function named "compareChoices" to compare both variables
-function compareChoices(){
-    if (userChoice === computerChoice){
-        console.log("The match is a Tie")
-    } else if (userChoice === "rock" && computerChoice === "paper"){
-        console.log("You have lost, Goodluck next time.")
-    } else if (userChoice === "rock" && computerChoice === "scissors"){
-        console.log("Congratulations, You have won")
-    } else if (userChoice === "paper" && computerChoice === "rock"){
-        console.log("Congratulations, You have won")
-    } else if (userChoice === "paper" && computerChoice === "scissors"){
-        console.log("You have lost, Goodluck next time.")
-    } else if (userChoice === "scissors" && computerChoice === "paper"){
-        console.log("Congratulations, You have won")
-    } else if (userChoice === "scissors" && computerChoice === "rock"){
-        console.log("You have lost, Goodluck next time.")
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return ("tie")
+    } else if (playerSelection === "rock" && computerSelection === "paper"){
+        return("lose")
+    } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return("win")
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+        return("win")
+    } else if (playerSelection === "paper" && computerSelection === "scissors"){
+        return("lose")
+    } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return("win")
+    } else if (playerSelection === "scissors" && computerSelection === "rock"){
+        return("lose")
     }
 }
 
-//IF userChoice = computerChoice
-//  output "The match is a tie"
-//else if userChoice = "rock" and computerChoice = "paper"
-//  output "User has lost"
-//else if userChoice = "rock" and computerChoice = "scissors"
-//  output "User has won"
-//else if userChoice = "paper" and computerChoice = "rock"
-//  output "User has won"
-//else if userChoice = "paper" and computerChoice = "scissors"
-//  output "User has lost"
-//else if userChoice = "scissors" and computerChoice = "paper"
-//  output "User has won"
-//else if userChoice = "scissors" and computerChoice = "rock"
-//  output "User has lost"
-//
+function game(){
+    let matchesWon = 0;
+    let matcheslost = 0;
+    let matchestied = 0;
+    for (let i = 0; i < 5; i++){
+        let round = playRound(getUserChoice(), getComputerChoice())
+        if (round === "win"){
+            matchesWon++;
+            console.log("congratulations, You have won!");
+        } else if (round === "lose"){
+            matcheslost++;
+            console.log("You have lost, Better luck next time.");
+        } else {
+            matchestied++;
+            console.log("Too bad, It was a tie.");
+        }
+    }
+    console.log(matchesWon);
+    console.log(matcheslost);
+    console.log(matchestied);
+}
+
+console.log(game())
