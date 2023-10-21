80f81d0db53c4a37a45f6a3ebada34ecd1659c84
SolitaryXero <103876121+SolitaryXero@users.noreply.github.com>
2023-10-21 11:59:49 +0500

    created UI for the game

--- script.js
+++ script.js
@@ -1,82 +1,115 @@
-//alternative branch
-//Create function named "getComputerChoice" to get Computer's choice
+//rps-ui branch
+
+let btnRock = document.querySelector(".btnRock");
+let btnPaper = document.querySelector(".btnPaper");
+let btnScissors = document.querySelector(".btnScissors");
+let results = document.querySelector(".results");
+
+let player = 0;
+let computer = 0;
+let ties = 0;
+
+round = btnRock.addEventListener("click", ()=>{
+    round = playRound("rock", getComputerChoice())
+    game(round);
+});
+btnPaper.addEventListener("click", ()=>{
+    round = playRound("paper", getComputerChoice())
+    game(round);
+});
+btnScissors.addEventListener("click", ()=>{
+    round = playRound("scissors", getComputerChoice())
+    game(round);
+});
+
+game();
+
 function getComputerChoice(){
-    //  Create variable "computerChoice" of type string
     let computerChoice = "";
-    //  Create variable "randomChoice", and give it random value between 1 and 3
     let randomChoice = Math.floor(Math.random()*3)+1;
     if(randomChoice === 1){
-        //IF randomChoice = 1, store "rock" in computerChoice
+        
         computerChoice = "rock";
     } else if (randomChoice === 2){
-        //ELSE IF randomChoice = 2, store "paper" in computerChoice
+        
         computerChoice = "paper";
     } else {
-        //ELSE randomChoice = 3, store "scissors" in computerChoice
+        
         computerChoice = "scissors"
     }
     return computerChoice;
 }
 
-//Create a function "getUserChoice"
-function getUserChoice(){
-    //Create a variable "userChoice"
-    //Prompt the User to choose between "rock", "paper" or "scissors"
-    let userChoice = prompt("Please choose one of the 3 options, Rock, Paper or scissors.").toLowerCase();
+function getUserChoice(choiceButtonClick){
+    if(!choiceButtonClick){
+        let userChoice = prompt("Please choose one of the 3 options, Rock, Paper or scissors.").toLowerCase();
     
-    //check if the user input is correct
-    //Return user's choice
-    if (userChoice === "rock"){
-        return "rock"
-    } else if (userChoice === "paper"){
-        return "paper"
-    } else if (userChoice === "scissors"){
-        return "scissors"
+        if (userChoice === "rock"){
+            return "rock"
+        } else if (userChoice === "paper"){
+            return "paper"
+        } else if (userChoice === "scissors"){
+            return "scissors"
+        } else {
+            results.textContent = "Invalid Input, Please refresh the page and play again.";
+        }
     } else {
-        console.log("Invalid Input, Please refresh the page and play again.");
+        return choiceButtonClick;
     }
+    
 }
 
-
-//Create a function named "compareChoices" to compare both variables
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
+        
         return ("tie")
     } else if (playerSelection === "rock" && computerSelection === "paper"){
+        
         return("lose")
     } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        
         return("win")
     } else if (playerSelection === "paper" && computerSelection === "rock"){
+        
         return("win")
     } else if (playerSelection === "paper" && computerSelection === "scissors"){
+        
         return("lose")
     } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        
         return("win")
     } else if (playerSelection === "scissors" && computerSelection === "rock"){
+        
         return("lose")
     }
 }
 
-function game(){
-    let matchesWon = 0;
-    let matcheslost = 0;
-    let matchestied = 0;
-    for (let i = 0; i < 5; i++){
-        let round = playRound(getUserChoice(), getComputerChoice())
-        if (round === "win"){
-            matchesWon++;
-            console.log("congratulations, You have won!");
-        } else if (round === "lose"){
-            matcheslost++;
-            console.log("You have lost, Better luck next time.");
-        } else {
-            matchestied++;
-            console.log("Too bad, It was a tie.");
+function game(round){
+    
+    console.log(round)
+    if (round === "win"){
+        player++;
+        
+
+    } else if (round === "lose"){
+        computer++;
+        
+
+    } else if (round === "tie") {
+        ties++;
+        
+    }
+
+    results.textContent = `Player: ${player}  Computer: ${computer}  Ties: ${ties}.`;
+
+    if (player+computer+ties === 5){
+        if (player > computer && player > ties){
+            results.textContent = "congratulations, You have won!";
+        } else if (computer > player && computer > ties){
+            results.textContent = "You have lost, Better luck next time.";
+        } else if (ties > player && ties > computer){
+            results.textContent = "Too bad, It was a tie.";    
         }
     }
-    console.log(matchesWon);
-    console.log(matcheslost);
-    console.log(matchestied);
 }
 
-console.log(game())
